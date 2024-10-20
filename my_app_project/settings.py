73c932cceb01adfd80bfb55f9f705d90c37ce916
Santhosh1507 <webdesign1507@gmail.com>
2024-10-20 15:46:28 +0530

    first commit

--- my_app_project/settings.py
+++ my_app_project/settings.py
@@ -73,10 +73,10 @@ WSGI_APPLICATION = "my_app_project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-"default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+# "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 
 DATABASES = {
     "default": {
